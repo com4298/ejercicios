0ac4e289404acc4b2f511779d26508d98277476f
com4298 <77255253+com4298@users.noreply.github.com>
2021-04-28 09:20:47 -0400

    Update main.py

--- c1e1/main.py
+++ c1e1/main.py
@@ -1,5 +1,5 @@
 # Escribe tu codigo aqui abajo
 nombre = "Clarisa Oquendo"
-Carrera = "Ing Mecanica y Electrica"
+carrera = "Ing Mecanica y Electrica"
 age = "23"
-Print("Hola mundo mi nombre es {nombre} y tengo {age} años, estudio {Carrera} en la UNIMET!") 
+print("Hola mundo mi nombre es {nombre} y tengo {age} años, estudio {Carrera} en la UNIMET!") 
